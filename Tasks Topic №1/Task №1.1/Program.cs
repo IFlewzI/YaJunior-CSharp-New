8fc4b8cd8e12d7b514d1c03b344daf17c4619d1e
Poltosik <stiff2006@mail.ru>
2024-04-22 00:56:48 +0300

    Completed a few tasks
    Tasks 1.1 and 1.3 are completed

---
+++ Tasks Topic №1/Task №1.1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task__1._1
+{
+    class Program
+    {
+        static void Main()
+        {
+            bool isAbleToTeleport = true;
+            string userName = "Test Name";
+            char userGender = 'm';
+            byte colorAlpha = 255;
+            ushort daysGone = 69;
+            short basicAttackDamage = -420;
+            uint playerExp = 78568;
+            int sleepImportanceValue = int.MinValue;
+            ulong cellsInHumanQuantity = 29934894205617;
+            float earthRadiusInKm = 6378.1f;
+            double earthEquatorLengthInKm = 40075.017d;
+        }
+    }
+}
---
+++ Tasks Topic №1/Task №1.3/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task__1._3
+{
+    class Program
+    {
+        static void Main()
+        {
+            string userInput;
+            bool isSuccess;
+
+            string userFirstName;
+            string userSecondName;
+            uint userAge;
+            string userFavouriteVideogame;
+            uint userRandomNumber;
+
+            Console.Write("Введите ваше имя. \nПоле для ввода: ");
+            userFirstName = Console.ReadLine();
+
+            Console.Write("\nВведите вашу фамилию. \nПоле для ввода: ");
+            userSecondName = Console.ReadLine();
+
+            Console.Write("\nВведите ваш возраст. \nПоле для ввода: ");
+            userInput = Console.ReadLine();
+            isSuccess = uint.TryParse(userInput, out userAge);
+
+            while (!isSuccess)
+            {
+                Console.Write("\nОшибка ввода. Повторите попытку. \nПоле для ввода: ");
+                userInput = Console.ReadLine();
+                isSuccess = uint.TryParse(userInput, out userAge);
+            }
+
+            Console.Write("\nКакая у вас любимая видеоигра? \nПоле для ввода: ");
+            userFavouriteVideogame = Console.ReadLine();
+
+            Console.Write("\nА назовёте первое пришедшее в голову неотрицательное число? \nПоле для ввода: ");
+            userInput = Console.ReadLine();
+            isSuccess = uint.TryParse(userInput, out userRandomNumber);
+
+            while (!isSuccess)
+            {
+                Console.Write("\nОшибка ввода. Повторите попытку. \nПоле для ввода: ");
+                userInput = Console.ReadLine();
+                isSuccess = uint.TryParse(userInput, out userRandomNumber);
+            }
+
+            Console.WriteLine($"\nПодводя итог: \nВас зовут {userSecondName} {userFirstName}, вам {userAge} лет. \nВаша любимая игра - это \"{userFavouriteVideogame}\", а ещё вы молодец {userRandomNumber} раз :)");
+            Console.ReadKey();
+        }
+    }
+}
