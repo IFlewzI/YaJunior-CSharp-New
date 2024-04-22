1e0f856800d3c27280f485906a5082c9cb433092
Poltosik <stiff2006@mail.ru>
2024-04-22 18:43:41 +0300

    Task 1.3 got fixed

--- Tasks Topic #1/Task #1.3/Program.cs
+++ Tasks Topic #1/Task #1.3/Program.cs
@@ -25,30 +25,22 @@ namespace Task__1._3
             Console.Write("\nВведите вашу фамилию. \nПоле для ввода: ");
             userSecondName = Console.ReadLine();
 
-            Console.Write("\nВведите ваш возраст. \nПоле для ввода: ");
-            userInput = Console.ReadLine();
-            isSuccess = uint.TryParse(userInput, out userAge);
-
-            while (!isSuccess)
+            do
             {
-                Console.Write("\nОшибка ввода. Повторите попытку. \nПоле для ввода: ");
+                Console.Write("\nВведите ваш возраст. \nПоле для ввода: ");
                 userInput = Console.ReadLine();
                 isSuccess = uint.TryParse(userInput, out userAge);
-            }
+            } while (isSuccess == false);
 
             Console.Write("\nКакая у вас любимая видеоигра? \nПоле для ввода: ");
             userFavouriteVideogame = Console.ReadLine();
 
-            Console.Write("\nА назовёте первое пришедшее в голову неотрицательное число? \nПоле для ввода: ");
-            userInput = Console.ReadLine();
-            isSuccess = uint.TryParse(userInput, out userRandomNumber);
-
-            while (!isSuccess)
+            do
             {
-                Console.Write("\nОшибка ввода. Повторите попытку. \nПоле для ввода: ");
+                Console.Write("\nА назовёте первое пришедшее в голову неотрицательное число? \nПоле для ввода: ");
                 userInput = Console.ReadLine();
                 isSuccess = uint.TryParse(userInput, out userRandomNumber);
-            }
+            } while (isSuccess == false);
 
             Console.WriteLine($"\nПодводя итог: \nВас зовут {userSecondName} {userFirstName}, вам {userAge} лет. \nВаша любимая игра - это \"{userFavouriteVideogame}\", а ещё вы молодец {userRandomNumber} раз :)");
             Console.ReadKey();
